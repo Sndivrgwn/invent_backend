2ae8b7c0c34363dc24c6aea0cbf52bdcc823173b
Wandi Apriansyah <wandiapriansyah.tkj@gmail.com>
2025-05-30 09:36:01 +0100

    feat: CRUD location

--- invent/routes/api.php
+++ invent/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LocationController;
 use App\Http\Controllers\RolesController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
@@ -16,3 +17,4 @@ Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, '
 Route::apiResource('roles', RolesController::class);
 Route::apiResource('users', UsersController::class);
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('locations', LocationController::class);
