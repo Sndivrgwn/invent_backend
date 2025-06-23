427e6a8737046254ac594af17317af6c5b889a06
Sndivrgwn <sandivirgiawan64@gmail.com>
2025-06-24 06:40:07 +0700

    NPM fix

--- invent/vite.config.js
+++ invent/vite.config.js
@@ -3,11 +3,38 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+    
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js'
+      ],
+      refresh: [
+        {
+          paths: ['resources/views/**'],
+          config: false,
+        },
+      ],
+    }),
+    tailwindcss({
+      config: './tailwind.config.js',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/resources/js',
+    },
+  },
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+      chunkFileNames: `chunks/[name]-[hash].js`,
+      assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  },
 });
